d3eecd4e8ed83e655d374fab3c6bf468857bf1bb
enrique Franco <andresmauro17>
2016-08-19 09:31:18 -0500

    modificamos este archivo para analizar la respuesta de los actuadores en impedancia

--- proc.m
+++ proc.m
@@ -26,7 +26,11 @@ for(k=1:Nf)
 	[amp1,idx1] = max(abs(ft1(1:32*1024)));
 	[amp2,idx2] = max(abs(ft2(1:32*1024)));
 	m(k) = amp2/amp1;
-	ph(k) = angle(ft1(idx1)) - angle(ft2(idx2));
+	ph(k) = -angle(ft1(idx1)) + angle(ft2(idx1));
+	am(k) = max(s2);
+	vr = am(k)*cos(ph(k));
+	vi = am(k)*sin(ph(k));
+	vm(k) = vr + j*vi;
 end
 
 f1 = 1e3;
@@ -34,15 +38,34 @@ f2 = 1000e3;
 hf = 1e3;
 f = [f1:hf:f2];
 
+Ve = 2.5;
+R = 127;
+Z = Ve./vm;
+
 subplot(2,1,1)
-plot(f/1e3,m)
+plot(f/1e3,am/R)
 xlabel('f (kHz)')
-ylabel('Amplitud')
+ylabel('Amplitud (A)')
+title('Corriente')
 grid
 subplot(2,1,2)
 plot(f/1e3,ph*180/pi)
 xlabel('f (kHz)')
 ylabel('Fase (grados)')
 grid
+
+figure
+subplot(2,1,1)
+semilogy(f/1e3,abs(Z))
+xlabel('f (kHz)')
+ylabel('Amplitud')
+title('Impedancia (Ohm)')
+grid
+subplot(2,1,2)
+plot(f/1e3,angle(Z)*180/pi)
+xlabel('f (kHz)')
+ylabel('Fase (grados)')
+grid
+
 
 
